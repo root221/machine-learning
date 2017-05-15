1534fd95b6474c976101d4ecf2faf47f74bdfb7a
skye <skye53653@gmail.com>
2017-05-16 02:02:35 +0900

    initial commit

---
+++ machine-learning-ex1/ex1/plotData.m
@@ -0,0 +1,9 @@
+function plotData(x, y)
+%PLOTDATA Plots the data points x and y into a new figure 
+%   PLOTDATA(x,y) plots the data points and gives the figure axes labels of
+%   population and profit.
+
+figure; % open a new figure window
+% plot(t,y2,'r') red color
+plot(x,y,'rx','MarkerSize',10);
+end
